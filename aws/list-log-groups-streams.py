916ab02c693923e76932e2c4c4db32dc3569ebd1
Artem Abramov <tematibr@gmail.com>
2020-07-14 04:50:21 +0300

    Simplify code to show log stream hierarchy

--- aws/list-log-groups-streams.py
+++ aws/list-log-groups-streams.py
@@ -1,93 +1,65 @@
 #!/usr/bin/env python3
+#
+# Test with
+# ./list-log-groups-streams.py
+#
 import boto3
-from datetime import datetime, date, time
-import botocore.session
 from botocore.config import Config
-import json
-
-from pprint import pprint
 import argparse
 from datetime import datetime
+from pprint import pprint
 
-parser = argparse.ArgumentParser(description='''
-Show the hierarchy Regions -> LogGroups -> LogStreams
-Credentials are read from ~/.aws/config or
-see https://boto3.amazonaws.com/v1/documentation/api/latest/guide/credentials.html
-''')
+def arguments():
+    parser = argparse.ArgumentParser(description='''
+    Show the hierarchy Regions -> LogGroups -> LogStreams
+    Credentials are read from ~/.aws/config or
+    see https://boto3.amazonaws.com/v1/documentation/api/latest/guide/credentials.html
+    ''')
 
-parser.add_argument('--prefixregion', type=str, default=None,
-    help='Prefix to filter regions e.g. us-east'
-)
-parser.add_argument('--prefixgroup', type=str, default=None,
-    help='Prefix to filter log-groups'
-)
-parser.add_argument('--prefixstream', type=str, default=None,
-    help='Prefix to filter log-streams'
-)
-parser.add_argument('-o', '--outdir', type=str, default="./",
-    help='Path to output directory, by default current directory'
-)
-parser.add_argument('-s', '--timestart', type=datetime.fromisoformat,
-    help='Start time for events in iso format e.g. 2020-07-14T00:33:24'
-)
-parser.add_argument('-e', '--timeend', type=datetime.fromisoformat,
-    help='End time for events in iso format e.g. 2020-07-14T00:33:24'
-)
+    parser.add_argument('-o', '--outdir', type=str, default="./",
+        help='Path to output directory, by default current directory'
+    )
 
-args = parser.parse_args()
+    args = parser.parse_args()
+    return args
 
 def list_regions():
-    # this can be any service, lets just use ec2
-    client = boto3.client('ec2')
+    # this must be 'ec2'
+    client = boto3.client( 'ec2' )
     responce = client.describe_regions()
-    # get only the names
-    # names = [region['RegionName'] if region['RegionName'].startswith(args.prefixregion) for region in responce['Regions']]
     names = [region['RegionName'] for region in responce['Regions']]
     return names
 
 def list_region_log_groups(client):
     responce = client.describe_log_groups(
-        # logGroupNamePrefix=args.prefixgroup,
-        limit=50
+            limit=50
     )
-    # get only the names
     names = [group['logGroupName'] for group in responce['logGroups']]
     return names
 
 def list_region_log_group_log_streams(client, log_group):
     responce = client.describe_log_streams(
         logGroupName=log_group,
-        # logStreamNamePrefix=args.prefixstream,
         orderBy='LogStreamName',
         descending=True,
         limit=50
     )
-    # get only the names
     names = [stream['logStreamName'] for stream in responce['logStreams']]
     return names
 
 def list_hierarchy():
+    res = {}
     for reg in list_regions():
-        # print(reg)
+        res[reg] = {}
         client = boto3.client('logs', region_name=reg)
         for group in list_region_log_groups(client):
-            # print("{}-{}".format(reg, group))
+            res[reg][group] = []
             for stream in list_region_log_group_log_streams(client, group):
-                # print("{}-{}-{}".format(reg, group, stream))
-                yield (reg, group, stream)
+                res[reg][group].append(stream)
+    return res
 
 if __name__=="__main__":
+    args = arguments()
     res = list_hierarchy()
-    for item in res:
-        print("{} - {:50} - {}".format(*item))
+    pprint(res)
 
-# def get_log_stream(client, log_group, log_stream)
-#     responce = client.get_log_events(
-#         logGroupName=log_group,
-#         logStreamName=log_stream,
-#         startTime=args.timestart,
-#         endTime=args.timeend
-#     )
-#     # write to file
-#     return responce
-# 
