241e42e63f99b8ce62320407d8d0bac867183ff0
Artem Abramov <tematibr@gmail.com>
2020-07-14 07:02:03 +0300

    Fix download-log-stream to remove bad chars from filename

--- aws/download-log-stream.py
+++ aws/download-log-stream.py
@@ -9,6 +9,7 @@ import argparse
 from datetime import datetime
 import os
 import json
+import re
 
 def arguments():
     parser = argparse.ArgumentParser(description='''
@@ -29,10 +30,10 @@ def arguments():
     parser.add_argument('-o', '--outdir', type=str, default="./",
         help='Path to output directory, by default current directory'
     )
-    parser.add_argument('-s', '--timestart', type=datetime.fromisoformat, default="3000-01-01T:00:00:00"
+    parser.add_argument('-s', '--timestart', type=datetime.fromisoformat, default=datetime.fromisoformat("3000-01-01T:00:00:00"),
         help='Start time for events in iso format e.g. 2020-07-14T00:33:24'
     )
-    parser.add_argument('-e', '--timeend', type=datetime.fromisoformat, default="2000-01-01T:00:00:00"
+    parser.add_argument('-e', '--timeend', type=datetime.fromisoformat, default=datetime.fromisoformat("2000-01-01T:00:00:00"),
         help='End time for events in iso format e.g. 2020-07-14T00:33:24'
     )
 
@@ -48,16 +49,24 @@ def open_aws_entity(args):
     client = boto3.client('logs', config=custom_config)
     return client
 
+def get_valid_fname(s):
+    s = str(s).strip()
+    s = s.replace('\\', '-')
+    s = s.replace('/', '-')
+    s = s.replace(' ', '-')
+    s = s.replace('.', '-')
+    return re.sub(r'(?u)[^-\w.]', '', s)
 
-def download_log_stream(local_dir, client, log_group, log_stream):
+def download_log_stream(local_dir, client, log_group, log_stream, tstart, tend):
     responce = client.get_log_events(
         logGroupName=log_group,
         logStreamName=log_stream,
-        startTime=int(args.timestart.timestamp()),
-        endTime=int(args.timeend.timestamp())
+        startTime=int(tstart.timestamp()),
+        endTime=int(tend.timestamp())
     )
-    stream_fs_name = "{}_{}_{}.log.json".format(client._client_config.region_name, log_group, log_stream)
-    dest_path = os.path.join(local_dir, stream_fs_name)
+    stream_fs_name = "{}_{}_{}".format(client._client_config.region_name, log_group, log_stream)
+    sanitised_stream_fs_name = get_valid_fname(stream_fs_name) + ".json"
+    dest_path = os.path.join(local_dir, sanitised_stream_fs_name)
     with open(dest_path, 'w') as stream_file:
         stream_file.write(json.dumps(responce))
 
@@ -65,4 +74,4 @@ def download_log_stream(local_dir, client, log_group, log_stream):
 if __name__=="__main__":
     args = arguments()
     client = open_aws_entity(args)
-    download_log_stream(args.outdir, client, args.log_group, args.log_stream)
+    download_log_stream(args.outdir, client, args.log_group, args.log_stream, args.timestart, args.timeend)
