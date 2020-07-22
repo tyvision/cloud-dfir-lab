6906d625ce35bd71c98dec77826c9373b78a52e7
Artem Abramov <tematibr@gmail.com>
2020-07-22 15:31:30 +0300

    Host terraform state in remote S3 bucket and fix recipie name

--- aws/terraform/cloud-dfir-lab.tf
+++ aws/terraform/cloud-dfir-lab.tf
@@ -2,10 +2,17 @@
 # for each stanza see the appropriate docs
 # e.g. for data "aws_ami" see https://www.terraform.io/docs/providers/aws/d/ami.html
 # e.g. for resource "aws_instance" see https://www.terraform.io/docs/providers/aws/r/instance.html
-#
 # to find possible AWS AMI see https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/finding-an-ami.html
-#
-# connect is something like: ssh -i "example_ec2_key.pem" ubuntu@ec2-52-202-17-121.compute-1.amazonaws.com
+# to connect is something like: ssh -i "example_ec2_key.pem" ubuntu@ec2-52-202-17-121.compute-1.amazonaws.com
+# terraform state is stored in s3 backend. See: https://www.terraform.io/docs/backends/types/s3.html
+
+terraform {
+  backend "s3" {
+    bucket = "terraform.testdom1.co.uk"
+    key    = "cloud-dfir-lab/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
 
 provider "aws" {
   profile = "default"
