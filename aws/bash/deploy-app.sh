bba11366ed5057244a654b36058d6777bb1123e8
Artem Abramov <tematibr@gmail.com>
2020-07-25 19:47:47 +0300

    Bash: deploy iptable rules to block access to unprotected docker container from outside

--- aws/bash/deploy-app.sh
+++ aws/bash/deploy-app.sh
@@ -32,4 +32,12 @@ EOF'
 # EOF'
 
 
+# # Optionally: block all connections except SSH
+# # Allow incoming and outgoing ssh only
+# iptables -A INPUT -p tcp -s 0/0 -d $SERVER_IP --sport 513:65535 --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
+# iptables -A OUTPUT -p tcp -s $SERVER_IP -d 0/0 --sport 22 --dport 513:65535 -m state --state ESTABLISHED -j ACCEPT
+#
+# # make sure nothing comes or goes out of this box
+# iptables -A INPUT -i eth0 -j DROP
+# iptables -A OUTPUT -o eth0 -j DROP
 
