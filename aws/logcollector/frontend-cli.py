78cae8b31c2c39cb28b54dc91b315fe214153ff8
Artem Abramov <tematibr@gmail.com>
2020-07-28 18:24:07 +0300

    Wrap frontend-cli code into one file

--- aws/logcollector/frontend-cli.py
+++ aws/logcollector/frontend-cli.py
@@ -7,10 +7,11 @@
 import os
 import shutil
 
+from pprint import pprint
+
 import importlib
 lib_utils = importlib.import_module("lib-utils")
-
-build_transfer_spec = importlib.import_module("build-transfer-spec")
+list_log_streams = importlib.import_module("list-log-stream")
 transfer_events = importlib.import_module("transfer-events")
 
 rawdir = './rawlogs/'
@@ -26,11 +27,89 @@ Interactive menu to choose and transfer events from AWS logs into Timesketch.
     ]
     return lib_utils.get_parser(helptext, args)
 
+
+def get_possible_download_items():
+    res = list_log_streams.list_hierarchy()
+    count = 0
+    valid = []
+    for reg, group_dict in res.items():
+        for group, stream_list in group_dict.items():
+            for stream in stream_list:
+                valid.append([" ", count, reg, group, stream])
+                count += 1
+    return valid
+
+
+def interactively_get_index(items):
+    for item in items:
+        print("{:2} {:3} {:20} {:60} {}".format(*item))
+
+    i = lib_utils.read_integer("Enter index of stream to download or 'q' to quit menu: ", 0, len(items)-1)
+    if not i:
+        return None
+
+    # change status
+    items[i][0] = "+"
+    return i
+
+
+def get_targets():
+    targets = {
+        "logstream" : []
+        , "s3" : []
+        , "metric" : []
+    }
+
+    while True:
+        print("Choose what type of logs you want to download")
+        type_index = lib_utils.read_integer("Choose menu number: 1=log-streams, 2=files in S3, 3=metrics or 'q' to quit menu: ", 1, 3)
+        if type_index is None:
+            break
+
+        if type_index == 1:
+            print("Decide what log stream you want")
+            print("Loading list of log streams in AWS...")
+            items = get_possible_download_items()
+
+            while True:
+                item_index = interactively_get_index(items)
+                if item_index is None:
+                    break
+
+                while True:
+                    plugin_list = list( transfer_events.cleanup_plugins.keys() )
+                    message = "Enter cleanup plugin for this log: {} or 'q' to quit menu: ".format(plugin_list)
+                    cleanup_plugin_name = lib_utils.read_string(message)
+                    if cleanup_plugin_name is None:
+                        break
+                    if cleanup_plugin_name not in plugin_list:
+                        print("Invalid name, choose one of {}".format(plugin_list))
+                        continue
+
+                    _, _, cur_reg, cur_group, cur_stream = items[item_index]
+
+                    targets["logstream"].append({
+                        "region" : cur_reg
+                        , "group" : cur_group
+                        , "stream" : cur_stream
+                        , "cleanup-plugin" : cleanup_plugin_name
+                    })
+                    break
+
+        elif type_index == 2:
+            print("Decide what S3 paths you want")
+
+        elif type_index == 3:
+            print("Decide what cloudwatch metrics you want")
+
+    print("You requested the following logs: ")
+    pprint(targets)
+    return targets
+
 def recreate_dir(path):
     if os.path.exists(path):
         shutil.rmtree(path)
     os.makedirs(path)
-
 
 if __name__ == "__main__":
     args = get_parser().parse_args()
@@ -38,7 +117,7 @@ if __name__ == "__main__":
     recreate_dir(rawdir)
     recreate_dir(cleandir)
 
-    targets = build_transfer_spec.get_targets()
+    targets = get_targets()
 
     # with open("../config/example-transfer-spec.json", "r") as f:
     #     import json
